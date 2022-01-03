f6290e5666fb263a71198faf9d3876ac277cb7d5
mikoa9 <klaryss_13@yahoo.fr>
2022-01-03 23:04:07 +0100

    Essai pour pousser flask comme outil en ligne de commande

--- api/api_flask_cl.py
+++ api/api_flask_cl.py
@@ -1,7 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+from app import app
+if __name__ == "__main__":
+  app.run()
