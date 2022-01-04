dd4eb9d21aa56c7c9ea8363ed06c0db65d4ca2d1
mikoa9 <klaryss_13@yahoo.fr>
2022-01-04 16:05:40 +0100

    sauvegarde

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from PIL import Image
 
-img = Image.open("https://www.faire-un-credit.fr/wp-content/uploads/2021/02/faire-un-credit-en-ligne.png") 
+#img = Image.open("https://www.faire-un-credit.fr/wp-content/uploads/2021/02/faire-un-credit-en-ligne.png") 
   
-st.image(img, width=200) 
+#st.image(img, width=200) 
 st.header("Scoring crédit client")
 st.subheader("Prédiction de la solvabilité d'un client pour l'obtention d'un crédit")
 st.text("Veuillez saisir l'identifiant du client")
@@ -26,3 +26,4 @@ feature = st.selectbox("features: ",
   
 st.write("Your feature: ", feature) 
 
+#faire une jauge
