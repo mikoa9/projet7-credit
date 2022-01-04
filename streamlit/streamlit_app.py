7cad48327c9c2b6d11f877dd5fdc4437866a74e9
mikoa9 <klaryss_13@yahoo.fr>
2022-01-04 15:31:56 +0100

    ajout image

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -4,8 +4,20 @@ from PIL import Image
 st.header("Scoring crédit client")
 st.subheader("Prédiction de la solvabilité d'un client pour l'obtention d'un crédit")
 st.text("Veuillez saisir l'identifiant du client")
-name = st.text_input( "", "Exemple d'identifiant : 122136...") 
+name = st.text_input(, "Exemple d'identifiant : 122136...") 
   
 if(st.button('Envoyez')): 
     result = name.title() 
     st.success(result) 
+
+st.success("Success") 
+  
+st.info("Information") 
+  
+st.warning("Warning") 
+  
+st.error("Error") 
+
+img = Image.open("https://www.faire-un-credit.fr/wp-content/uploads/2021/02/faire-un-credit-en-ligne.png") 
+  
+st.image(img, width=200) 
