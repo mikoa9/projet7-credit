38c27ae1eb575c1d387bce0d06a12d9908b44da5
mikoa9 <klaryss_13@yahoo.fr>
2022-01-04 15:25:50 +0100

    modif champ

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.header("Scoring crédit client")
 st.subheader("Prédiction de la solvabilité d'un client pour l'obtention d'un crédit")
 st.text("Veuillez saisir l'identifiant du client")
-name = st.text_input( "Exemple d'identifiant : 122136...") 
+name = st.text_input( "", "Exemple d'identifiant : 122136...") 
   
 if(st.button('Envoyez')): 
     result = name.title() 
