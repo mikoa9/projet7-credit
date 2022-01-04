dbb60dab81a043cb60606d328605b2c4e1b1ef28
mikoa9 <klaryss_13@yahoo.fr>
2022-01-04 15:40:58 +0100

    correction erreur synthaxe champ

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -7,7 +7,7 @@ st.image(img, width=200)
 st.header("Scoring crédit client")
 st.subheader("Prédiction de la solvabilité d'un client pour l'obtention d'un crédit")
 st.text("Veuillez saisir l'identifiant du client")
-name = st.text_input(, "Exemple d'identifiant : 122136...") 
+name = st.text_input("", "Exemple d'identifiant : 122136...") 
   
 if(st.button('Envoyez')): 
     result = name.title() 
