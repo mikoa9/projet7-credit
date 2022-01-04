bdd4ef4a1bef888cbac6a04a01272aa0a5d152b0
mikoa9 <klaryss_13@yahoo.fr>
2022-01-04 22:43:44 +0100

    Ajouter un objet intermédiaire

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+model = None
 
 @app.route("/")
 def running():
@@ -8,8 +9,12 @@ def running():
 
 @app.route("/predict/<client_id>")
 def predict(client_id):
-  c = { "o": client_id }
+  c = { "o": client_id, "model":model }
   return c
+
+@app.before_first_request
+def load_model():
+  model = 1
 
 if __name__ == "__main__":
   app.run(threaded=True, port=5000)
