a9a6283dc20c3b0536ba1e2591d5423af9472c9b
mikoa9 <klaryss_13@yahoo.fr>
2022-01-04 20:49:45 +0100

    Accepter param pour l'endpoint predict

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def running():
+    return "<p>Serveur en route...</p>"
+
+@app.route("/predict/<client_id>")
+def predict(client_id):
+  return { c:client_id };
 
 if __name__ == "__main__":
   app.run(threaded=True, port=5000)
